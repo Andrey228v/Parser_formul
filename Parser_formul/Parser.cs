d8e33c384f1fcc9bfd99efcbeb5e7c709110aab3
gysev003 gysev003 <gysev003@mail.ru>
2023-01-05 08:23:52 +0300

    Добавьте файлы проекта.

---
+++ Parser_formul/Parser.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parser_formul
+{
+    class Parser
+    {
+
+        // Глобальные переменные, вопрос о рациональности... 
+        List<double> list_chisel = new List<double>(); // Лист хранения чисел
+        List<string> list_razbienia_po_skobkam = new List<string>(); // Лист хранения разбиения скобок
+        List<string> list_operacii = new List<string>(); // Лист очереди операций
+        List<string> preoritet_operazii = new List<string>() { "log", "sqrt", "^", "/", "*", "+", "-" };
+        string otvet; // Результат
+        int otsechka_prohoda = 0; // Проход по строке
+
+
+        public Parser()
+        {
+           
+        }
+
+
+        // Функция по преобразованию строки в форумул
+        // Функция получает на входе строку stroka;
+        // Функция получает на входе параметры.
+        public void Preobrazovanie_str_v_formulu(string stroka, Dictionary<string, double> parametri)
+        {
+
+            Console.WriteLine($"stroka: {stroka}");
+            foreach (var parametr in parametri)
+            {
+                Console.WriteLine($"parametr:{parametr}");
+            }
+
+            Razbienie_stroki(stroka);
+
+
+        }
+
+        private void Razbienie_stroki(string stroka)
+        {
+
+            //otsechka_prohoda
+            for (int i = 0; i < stroka.Length; i += 1)
+            {
+                string simvol = stroka[i].ToString();
+
+                // Разбиение по скобкам
+                if (simvol == "(")
+                {
+                    Poisk_skobok_v_glubinu(stroka);
+                }
+
+                otsechka_prohoda += 1; // тут не единичка ???
+
+            }
+
+
+        }
+
+        private void Poisk_skobok_v_glubinu(string stroka)
+        {
+            int nachalo_prohoda = otsechka_prohoda;
+            int konec_prohoda;
+
+            for (int i = nachalo_prohoda; i < stroka.Length; i += 1)
+            {
+                Console.WriteLine($"i:{i}");
+
+                string simvol = stroka[i].ToString();
+
+                // Разбиение по скобкам
+                if (simvol == "(")
+                {
+                    otsechka_prohoda += 1;
+                    Poisk_skobok_v_glubinu(stroka);
+                }
+
+                if (simvol == ")")
+                {
+                    konec_prohoda = i;
+                    Console.WriteLine($"nachalo_prohoda:{nachalo_prohoda}, konec_prohoda:{konec_prohoda}");
+                    Console.WriteLine($"Срез строки поиска скобок в глубину:{stroka.Substring(nachalo_prohoda, stroka.Length-konec_prohoda)}");
+                }
+
+                
+
+            }
+
+
+        }
+
+
+        private void Razbienie_prosoi_stroki(string chast_stroki)
+        {
+
+        }
+
+        // Функция, которая должна считать простые математические действия
+        // На вход передаётся часть строки, которая заключена в скобки
+        private void Prostoi_raschet()
+        {
+
+
+
+        }
+
+
+
+
+
+    }
+}
---
+++ Parser_formul/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Parser_formul
+{
+    class Program
+    {
+       
+
+        // Проблемы :
+        // Могут быть отрицательные числа
+        // Возведение в степень может быть ^, а может быть ** надо выбрать что-то одно.
+
+        static void Main(string[] args)
+        {
+            Parser Parser_1 = new Parser();
+
+            ////Test_2
+            string stroka_2_test = "(4+4)*10+5"; // ответ должен быть 80;
+            Dictionary<string, double> parametri_2_test = new Dictionary<string, double>() { { "a", 12.2 }, { "b", 12.4 } };
+            Parser_1.Preobrazovanie_str_v_formulu(stroka_2_test, parametri_2_test);
+
+
+            //Test_1
+            //string stroka_1_test = "2+5"; // ответ должен быть 7;
+            //Dictionary<string, double> parametri_1_test = new Dictionary<string, double>() { {"a", 12.2 }, {"b", 12.4 } };
+            //Parser_1.Preobrazovanie_str_v_formulu(stroka_1_test, parametri_1_test);
+
+
+
+
+        }
+    }
+}
