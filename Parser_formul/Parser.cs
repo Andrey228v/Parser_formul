900d91d48ec322b906d3cf7c6d5b5ce8296f885e
gysev003 gysev003 <gysev003@mail.ru>
2023-01-05 10:36:58 +0300

    Разбиение скобочек

--- Parser_formul/Parser.cs
+++ Parser_formul/Parser.cs
@@ -14,8 +14,9 @@ namespace Parser_formul
         List<string> preoritet_operazii = new List<string>() { "log", "sqrt", "^", "/", "*", "+", "-" };
         string otvet; // Результат
         int otsechka_prohoda = 0; // Проход по строке
-
-
+        int glubina = 1; // Глубина погружения для тестов
+        static bool stop_metka_dli_testov = false; // Для тестов
+       
         public Parser()
         {
            
@@ -51,6 +52,10 @@ namespace Parser_formul
                 if (simvol == "(")
                 {
                     Poisk_skobok_v_glubinu(stroka);
+                    //Заглушка
+                    Console.WriteLine($"ii:{i}");
+                    Console.WriteLine($"stroka posle obrabotki: {stroka}");
+                    break;
                 }
 
                 otsechka_prohoda += 1; // тут не единичка ???
@@ -60,14 +65,32 @@ namespace Parser_formul
 
         }
 
+
+        //Передаётся строка, ограниченная с двух сторон скобками
+        //Передаётся глубина погружения, чтобы для тестов можно было выплывать....
         private void Poisk_skobok_v_glubinu(string stroka)
         {
             int nachalo_prohoda = otsechka_prohoda;
             int konec_prohoda;
+            string srez_stroki_so_skobkami; // Переменная среза со скобками
+            string srez_stroki_bez_skobok; // Вопрос может срез может быть и без скобок... 
 
-            for (int i = nachalo_prohoda; i < stroka.Length; i += 1)
+            
+
+            Console.WriteLine($"otsechka_prohoda:{otsechka_prohoda}");
+
+            // Ставим +1 потому что надо смотреть от скобки, которая была найдена за шаг до предыдущей
+            // Вопрос корректно ли это... 
+            for (int i = nachalo_prohoda + 1; i < stroka.Length; i += 1)
             {
-                Console.WriteLine($"i:{i}");
+
+                // Заглушка
+                if (stop_metka_dli_testov == true)
+                {
+                    Console.WriteLine($"Есть заход");
+                    return;
+                }
+
 
                 string simvol = stroka[i].ToString();
 
@@ -75,26 +98,63 @@ namespace Parser_formul
                 if (simvol == "(")
                 {
                     otsechka_prohoda += 1;
+                    glubina += 1;
                     Poisk_skobok_v_glubinu(stroka);
                 }
 
                 if (simvol == ")")
                 {
+                    Console.WriteLine(stop_metka_dli_testov);
+                    stop_metka_dli_testov = true;
                     konec_prohoda = i;
                     Console.WriteLine($"nachalo_prohoda:{nachalo_prohoda}, konec_prohoda:{konec_prohoda}");
-                    Console.WriteLine($"Срез строки поиска скобок в глубину:{stroka.Substring(nachalo_prohoda, stroka.Length-konec_prohoda)}");
+                    //Вопрос верная ли формула Длина_Строки-Конец_прохода-1
+                    //Нет не верная, необходимо добавить вычитание начала отсчёта, потому что скобка может быть не в начале...
+                    Console.WriteLine($"Срез строки поиска скобок в глубину:{stroka.Substring(nachalo_prohoda, konec_prohoda - nachalo_prohoda + 1)}");
+
+                    //Задача- вычислить то что мы нашли в скобках в глубине
+                    //Заменить ответом выражение в скобках ответом, который мы получили.
+                    // Мы получаем простую строку, которую надо вычислить
+                    srez_stroki_so_skobkami = stroka.Substring(nachalo_prohoda, konec_prohoda - nachalo_prohoda + 1);
+                    srez_stroki_bez_skobok = stroka.Substring(nachalo_prohoda + 1, konec_prohoda - nachalo_prohoda );
+                    // Тут надо тестировать что передавать со скобками или без
+                    Razbienie_prosoi_stroki(srez_stroki_bez_skobok);
+                    
+                    
+                    // Поставим тут заглушку.
+                    break;
                 }
 
                 
-
             }
-
-
         }
 
 
-        private void Razbienie_prosoi_stroki(string chast_stroki)
+        //Данная функция на выходе получает простой ряд без скобок, в результате должный вернуть строку с ответом
+        //, который был в данных скобках
+        private string Razbienie_prosoi_stroki(string chast_stroki)
         {
+            string rez_vichislenia = "";
+            string chislo = "";
+            string operacia = "";
+
+            // К примеру, поступило 2+2.1+4/5
+            // Разобьём строку по числам и знакам по приоритету
+            // На выходе должен получится ответ данного выражения
+            // А если отрицательное число ... 
+            // [2, 2.1, 4, 5]
+            // [+, +, /]
+            Console.WriteLine($"chast_stroki: {chast_stroki}");
+
+            for(int i =0; i < chast_stroki.Length; i += 1)
+            {
+
+
+                //list_chisel.Add();
+                //list_operacii.Add();
+            }
+
+            return rez_vichislenia;
 
         }
 
--- Parser_formul/Program.cs
+++ Parser_formul/Program.cs
@@ -16,7 +16,7 @@ namespace Parser_formul
             Parser Parser_1 = new Parser();
 
             ////Test_2
-            string stroka_2_test = "(4+4)*10+5"; // ответ должен быть 80;
+            string stroka_2_test = "5+10*(((((-4+5+4.001))+111)*10)+5)+10"; // ответ должен быть ....;
             Dictionary<string, double> parametri_2_test = new Dictionary<string, double>() { { "a", 12.2 }, { "b", 12.4 } };
             Parser_1.Preobrazovanie_str_v_formulu(stroka_2_test, parametri_2_test);
 
